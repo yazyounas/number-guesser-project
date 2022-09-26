0c8253ce297b7a47977f056dcc75611b9e9e814c
Yasir Younas <yazyounas@gmail.com>
2022-09-26 21:07:11 +0100

    wrote functions to guess the secrete number

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+// creating function that genrate randome number 
+
+let generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+console.log(generateTarget());
+//create function that compare the closest target
+
+let compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+    if (humanDifference <= computerDifference) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+// score increase by 1
+let updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++
+    }else if (winner === 'computer') {
+        computerScore++
+    }
+}
+
+let advanceRound = () => currentRoundNumber++;
+   
 
 
 
